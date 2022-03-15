2b25e9db3eb40326914de4919af7f3a5ba07d980
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-03-15 16:11:58 +0530

    added icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    
+    <Sidebar />
   );
 }
 
---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import "./sidebar.css"
+import SidebarLink from "./SidebarLink"
+import HomeIcon from "@material-ui/icons/Home"
+import SearchIcon from "@material-ui/icons/Search"
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone"
+import MailOutlineIcon from "@material-ui/icons/MailOutline"
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder"
+import ListAltIcon from "@material-ui/icons/ListAlt"
+import PermIdentityIcon from "@material-ui/icons/PermIdentity"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <SidebarLink text="Home" Icon={HomeIcon}/>
+            <SidebarLink text="Explore" Icon={SearchIcon}/>
+            <SidebarLink text="Notifications" Icon={NotificationsNoneIcon}/>
+            <SidebarLink text="Messages" Icon={MailOutlineIcon}/>
+            <SidebarLink text="Bookmarks" Icon={BookmarkBorderIcon}/>
+            <SidebarLink text="Lists" Icon={ListAltIcon}/>
+            <SidebarLink text="Profile" Icon={PermIdentityIcon}/>
+            <SidebarLink text="More" Icon={MoreHorizIcon}/>
+        </div>
+    )
+}
+
+export default Sidebar
